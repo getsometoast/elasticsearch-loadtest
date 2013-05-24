3692e9c8ddd1c10102205413404d3a3b0cec985e
unknown <jamesl@jlewis-winvm.7digital.local>
2013-05-24 17:19:41 +0100

    add command line arguments

--- elasticsearch-loadtest-app/Program.cs
+++ elasticsearch-loadtest-app/Program.cs
@@ -1,7 +1,20 @@
-﻿namespace elasticsearch_loadtest_app
+﻿using System.Collections.Generic;
+using System.Configuration;
+
+namespace elasticsearch_loadtest_app
 {
 	class Program
 	{
+		private static string _maxThreads;
+		private static string _typeName;
+		private static string _indexName;
+		private static string _host;
+		private static string _dataPath;
+		private static string _batchSize;
+		private static string _shards;
+		private static string _replicas;
+		private static string _refreshInterval;
+
 		// #######################################################
 		// 
 		// Available Options: 
@@ -18,7 +31,70 @@
 		// #######################################################
 		static void Main(string[] args)
 		{
+			SetDefaultParameters();
 
+			if (args.Length > 0)
+				SetUserDefiniedParameters(args);
+		}
+
+		private static void SetDefaultParameters()
+		{
+			_maxThreads = ConfigurationManager.AppSettings["Default.MaxThreads"];
+			_indexName = ConfigurationManager.AppSettings["Default.IndexName"];
+			_typeName = ConfigurationManager.AppSettings["Default.TypeName"];
+			_host = ConfigurationManager.AppSettings["Default.Host"];
+			_dataPath = ConfigurationManager.AppSettings["Default.DataPath"];
+			_batchSize = ConfigurationManager.AppSettings["Default.BatchSize"];
+			_shards = ConfigurationManager.AppSettings["Default.Shards"];
+			_replicas = ConfigurationManager.AppSettings["Default.Replicas"];
+			_refreshInterval = ConfigurationManager.AppSettings["Default.RefreshInterval"];
+		}
+
+		private static void SetUserDefiniedParameters(string[] args)
+		{
+			var arguments = new Dictionary<string, string>();
+			
+			foreach (var arg in args)
+			{
+				var argValArray = arg.Split(':');
+				arguments.Add(argValArray[0], argValArray[1]);
+			}
+
+			foreach (var argument in arguments)
+			{
+				switch (argument.Key)
+				{
+					case "/host":
+						_host = argument.Value;
+						break;
+					case "/index-name":
+						_indexName = argument.Value;
+						break;
+					case "/type-name":
+						_typeName = argument.Value;
+						break;
+					case "/max-threads":
+						_maxThreads = argument.Value;
+						break;
+					case "/data-path":
+						_dataPath = argument.Value;
+						break;
+					case "/batch-size":
+						_batchSize = argument.Value;
+						break;
+					case "/shards":
+						_shards = argument.Value;
+						break;
+					case "/replicas":
+						_replicas = argument.Value;
+						break;
+					case "/refresh-interval":
+						_refreshInterval = argument.Value;
+						break;
+					default:
+						break;
+				}
+			}
 		}
 	}
 }

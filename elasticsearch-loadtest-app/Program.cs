0fdc2c33fe13b45d03dd95e4fcc4586be35405d8
unknown <jamesl@jlewis-winvm.7digital.local>
2013-05-24 15:52:09 +0100

    add app config with some default command line arguments add a default document to hit elasticsearch with

--- elasticsearch-loadtest-app/Program.cs
+++ elasticsearch-loadtest-app/Program.cs
@@ -1,14 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace elasticsearch_loadtest_app
+﻿namespace elasticsearch_loadtest_app
 {
 	class Program
 	{
+		// #######################################################
+		// 
+		// Available Options: 
+		//	/host: host address e.g. http://localhost:9200
+		//	/index-name: index name e.g. tweet
+		//	/type-name: type name e.g. type1
+		//	/max-threads: maximum threads for parallel loop
+		//	/data-path: file path to a data file to use as the document in the load test
+		//	/batch-size: size of a bulk indexing request
+		//	/shards: initial number of shards in the index
+		//	/replicas: initial number of replicas
+		//	/refresh-interval: initial index refresh interval
+		// 
+		// #######################################################
 		static void Main(string[] args)
 		{
+
 		}
 	}
 }
